e6864c4cc8c469ac18806a26f25b5963832d6602
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-03-01 12:07:04 +0530

    Add a camera to the car

--- loading_car.py
+++ loading_car.py
@@ -11,4 +11,14 @@ vehicle_bp = blueprints.find('vehicle.tesla.model3')
 vehicle = world.try_spawn_actor(vehicle_bp, random.choice(spawn_points))
 
 for i in range(30):
-    vehicle_bp = random.choice(blueprints.filter('vehicle'))
+    vehicle_bp = random.choice(blueprints.filter('vehicle*'))
+    npc = world.try_spawn_actor(vehicle_bp, random.choice(spawn_points))
+
+for v in world.get_actors().filter('vehicle*'):
+    v.set_autopilot(True)
+
+camera_bp = blueprints.find('sensor.camera.rgb')
+camera_init_trans = carla.Transform(carla.Location(z=2))
+camera = world.spawn_actor(camera_bp, camera_init_trans, attach_to=vehicle)
+camera.listen(lambda data: data.save_to_disk('out/%.6d.png' % data.frame))
+#camera.stop()
